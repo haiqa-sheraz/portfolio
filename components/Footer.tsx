7a3ec384ffa2723754fb2d99ed2b5678eed3ed92
haiqa-sheraz <haiqa.sheraz@gmail.com>
2025-05-26 19:23:24 +0300

    version update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import { FaLocationArrow } from "react-icons/fa6";
 
 import { socialMedia } from "@/data";
 import MagicButton from "./ui//MagicButton";
-
+import Image from "next/image";
 
 const Footer = () => {
   return (
@@ -37,7 +37,7 @@ const Footer = () => {
         rel="noopener noreferrer" // Security best practices
         className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
       >
-        <img src={info.img} alt="icons" width={20} height={20} />
+        <Image src={info.img} alt="icons" width={20} height={20} />
       </a>
     ))}
   </div>
